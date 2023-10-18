7f3b113bc3f7569d27457c6d16a933aed6dee5eb
Asanka Abeyweera <asankaab@wso2.com>
2023-10-18 11:29:00 +0530

    Add debug logs

--- .azure/process-metadata.py
+++ .azure/process-metadata.py
@@ -3,18 +3,22 @@ import yaml
 import json
 import shutil
 
-REPO_BASE_DIR = '..' 
-BUILD_STAGING_DIRECTORY = os.environ['BUILD_STAGINGDIRECTORY']  # Azure DevOps pipeline environment variable
+REPO_BASE_DIR = '..'  # One level up from the .azure directory to get to the repo root
+BUILD_STAGING_DIRECTORY = os.environ['BUILD_STAGINGDIRECTORY']
 
 def collect_metadata_and_thumbnails():
     collected_data = []
+    print("Starting to collect metadata and thumbnails...")
 
     # Iterate through directories and collect metadata from metadata.yaml
     for directory in os.listdir(REPO_BASE_DIR):
         dir_path = os.path.join(REPO_BASE_DIR, directory)
         metadata_file = os.path.join(dir_path, 'metadata.yaml')
+
+        print(f"Checking directory: {directory}")
         
         if os.path.isdir(dir_path) and os.path.exists(metadata_file):
+            print(f"Found metadata.yaml in directory: {directory}")
             with open(metadata_file, 'r') as f:
                 data = yaml.safe_load(f)
                 collected_data.append(data)
@@ -24,7 +28,10 @@ def collect_metadata_and_thumbnails():
             thumbnail_dest_folder = os.path.join(BUILD_STAGING_DIRECTORY, directory)
             os.makedirs(thumbnail_dest_folder, exist_ok=True)
             if os.path.exists(thumbnail_src):
+                print(f"Copying thumbnail for {directory}")
                 shutil.copy(thumbnail_src, thumbnail_dest_folder)
+            else:
+                print(f"Thumbnail not found for {directory}")
 
     return collected_data
 
@@ -37,9 +44,14 @@ def generate_index_json(data):
 
     with open(os.path.join(BUILD_STAGING_DIRECTORY, 'index.json'), 'w') as f:
         json.dump(index_data, f, indent=2)
+    print("Generated index.json")
 
 def main():
     metadata_data = collect_metadata_and_thumbnails()
+    if metadata_data:
+        print(f"Collected data for {len(metadata_data)} samples.")
+    else:
+        print("No metadata collected!")
     generate_index_json(metadata_data)
 
 if __name__ == '__main__':
