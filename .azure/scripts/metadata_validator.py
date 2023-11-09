2faf033bc67a59563d48cfff6b85834643db2506
Chameera Rupasinghe <chameerarupasinghe@gmail.com>
2023-11-09 15:20:56 +0530

    add angular to buildpack list

--- .azure/scripts/metadata_validator.py
+++ .azure/scripts/metadata_validator.py
@@ -14,7 +14,7 @@ VALID_COMPONENT_TYPES = [
 
 VALID_BUILD_PACKS = [
     "ballerina", "wso2-mi", "go", "java", "php", "python", "nodejs", "ruby", 
-    "many", "postman", "react", "docker"
+    "many", "postman", "react", "docker", "angular"
 ]
 
 def validate_component_path(component_path, repository_url):
