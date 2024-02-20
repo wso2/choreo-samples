77ac1e03b2225560c65c25be86ddafc1e4075a16
Thushara Sampath <mkt.sampath@yahoo.com>
2024-02-20 12:31:09 +0530

    Update samples to works with `Run with Arguments`

--- docker-hello-world-manual-task/main.go
+++ docker-hello-world-manual-task/main.go
@@ -18,8 +18,14 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	for i, arg := range os.Args[1:] {
+		fmt.Println("Arg", i, ": ", arg)
+	}
 }
---
+++ hello-world-nodejs-task/index.js
@@ -0,0 +1,3 @@
+console.log('Hello World!');
+const args = process.argv.slice(2); // slice out the first two sys args
+console.log(args);
