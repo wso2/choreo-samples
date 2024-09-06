d7e148f7497d85087341e602c1e2ba7c488de411
chathuranga95 <siriwardhanachathuranga@gmail.com>
2024-09-06 16:51:35 +0530

    Add prism mock service sample metadata

--- .azure/scripts/metadata_validator.py
+++ .azure/scripts/metadata_validator.py
@@ -14,7 +14,7 @@ VALID_COMPONENT_TYPES = [
 
 VALID_BUILD_PACKS = [
     "ballerina", "wso2-mi", "go", "java", "php", "python", "nodejs", "ruby",
-    "vue", "many", "postman", "react", "docker", "angular", "dotnet"
+    "vue", "many", "postman", "react", "docker", "angular", "dotnet", "prism-mock"
 ]
 
 def validate_component_path(component_path, repository_url):
