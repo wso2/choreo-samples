4ce9255ed181c9f3c0e53db9a618be4ff754ae9e
Gayantha Sandeep Dassanayake <115539762+G5andeepD@users.noreply.github.com>
2024-10-28 10:44:48 +0530

    Add acr push logic for samples (#164)
    * Add acr push logic for samples
    
    * Remove unwanted comma
    
    * Fix Docker file
    
    * fix vue docker
    
    * Add dockerfile for react spa
    
    * Fix vue dockerfile
    
    * fix vue dockerfile
    
    * fix vue conf file
    
    * fix react dockerfile
    
    * use unprivilaged nginx image
    
    * Fix
    
    * Fix
    
    * fix
    
    * Fix port
    
    * Fix port
    
    * Fix docker files very properly
    
    * add checkov ignore to angular spa
    
    * Checkov skip 3
    
    * create non root user
    
    * use non root user
    
    * fix user num
    
    * Fix add user
    
    * Use 10014
    
    * Fix spa dockerfiles with 10014 user
    
    * Fix spa dockerfiles with 10014 user
    
    * Remove checkov ignore
    
    * Remove nginx conf
    
    * Fix trivy issue for manual task
    
    * Fix trivy again
    
    * Fix trivy issue on docker scheduled task
    
    * Fix trivy vul in docker rest user service
    
    * apply suggestions
    
    * Apply suggestions
    
    * [TEMP] try nginx alpine for vue
    
    * [TEMP] Try with more permission
    
    * [TEMP] Use nginx unprivilaged
    
    * [TEMP] Try node
    
    * [TEMP] Revert
    
    * Update nginx image

--- .azure/scripts/metadata_validator.py
+++ .azure/scripts/metadata_validator.py
@@ -5,12 +5,16 @@ import shutil
 
 REPO_BASE_DIR = os.environ['BUILD_SOURCESDIRECTORY']
 WSO2_SAMPLES_REPO_URL = 'https://github.com/wso2/choreo-samples/'
-CHOREO_ACR_BASE_URL = 'choreoanonymouspullable.azurecr.io'
 
 VALID_COMPONENT_TYPES = [
     "service", "webhook", "manual-task", "scheduled-task", 
     "event-triggered", "event-handler", "test-runner", "many",
     "web-application", "git-proxy"
+]
+
+QUICK_DEPLOYABLE_COMPONENT_TYPES = [
+    "service", "manual-task", "scheduled-task", 
+    "web-application"
 ]
 
 VALID_BUILD_PACKS = [
@@ -50,5 +54,5 @@ def validate_thumbnail(thumbnail_src):
         return False
     return True
 
-def validate_image_url(image_url):
-    return image_url.startswith(CHOREO_ACR_BASE_URL)
+def is_component_type_quick_deployable(component_type):
+    return component_type in QUICK_DEPLOYABLE_COMPONENT_TYPES
--- .azure/scripts/process_metadata.py
+++ .azure/scripts/process_metadata.py
@@ -7,9 +7,11 @@ import json
 import yaml
 import metadata_validator
 import version_manager
+import subprocess
 
 REPO_BASE_DIR = os.environ['BUILD_SOURCESDIRECTORY']
 BUILD_STAGING_DIRECTORY = os.environ['BUILD_STAGINGDIRECTORY']
+CHOREO_ACR_BASE_URL = 'choreoanonymouspullable.azurecr.io'
 BASE_URL_FOR_THUMBNAILS = 'https://choreo-shared-choreo-samples-cdne.azureedge.net'
 SAMPLE_COMPONENT_TYPE_SERVICE = 'service'  
 
@@ -46,11 +48,38 @@ def collect_metadata_and_thumbnails():
                     continue
 
                 # check if a quick deployable sample has a valid image URL
-                image_url = data.get('imageUrl')
-                if image_url:
-                    if not metadata_validator.validate_image_url(image_url):
-                        print(f"Warning: 'imageUrl' is not a valid image URL for the sample: {meta_file}. Excluding from index.json.")
+                image_version = data.get('imageVersion')
+                if image_version:
+
+                    if not metadata_validator.is_component_type_quick_deployable(component_type):
+                        print(f"Warning: '{component_type}' is not a quick deployable component type for directory: {meta_file}. Excluding from index.json.")
                         continue
+
+                    display_name = data.get('displayName')
+                    image_name = display_name.strip().lower().replace(' ', '-')
+                    image_url = f"{CHOREO_ACR_BASE_URL}/samples/{image_name}:{image_version}"
+                    data['imageUrl'] = image_url
+
+                    # Attempt to pull the image from ACR
+                    pull_command = ['docker', 'pull', image_url]
+                    pull_result = subprocess.run(pull_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    if pull_result.returncode == 0:
+                        print(f"Image {image_url} exists in ACR.")
+                    else:
+                        print(f"Image {image_url} not found in ACR. Building and pushing the image.")
+
+                        # Build the Docker image
+                        component_full_path = os.path.join(REPO_BASE_DIR, component_path.lstrip('/'))
+                        build_command = ['docker', 'build', '-t', image_url, component_full_path]
+                        build_result = subprocess.run(build_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        if build_result.returncode != 0:
+                            raise RuntimeError(f"Error building image {image_url}: {build_result.stderr.decode('utf-8')}")
+
+                        # Push the image to ACR
+                        push_command = ['docker', 'push', image_url]
+                        push_result = subprocess.run(push_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        if push_result.returncode != 0:
+                            raise RuntimeError(f"Error pushing image {image_url}: {push_result.stderr.decode('utf-8')}")
 
                     # Check if openapi.yaml and endpoints.yaml exist if the component type is a service
                     if component_type == SAMPLE_COMPONENT_TYPE_SERVICE:
@@ -104,7 +133,7 @@ def sort_samples(samples):
     rest_samples = []
 
     for sample in samples:
-        if sample.get('imageUrl'):
+        if sample.get('imageVersion'):
             sample_type = sample.get("componentType", "")
             if sample_type in type_order:
                 qd_samples_by_type[sample_type].append(sample)
