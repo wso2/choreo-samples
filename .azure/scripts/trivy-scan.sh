cacb2e2e03e7b39def7d32423c7642acab63a828
G5andeepD <e19063@eng.pdn.ac.lk>
2024-10-30 14:34:17 +0530

    Try to induce vul and fail

--- .azure/scripts/trivy-scan.sh
+++ .azure/scripts/trivy-scan.sh
@@ -5,24 +5,28 @@ trivy_scan() {
   image=$1
   echo "[INFO] Scanning image: $image"
   
-  if [[ "$push" == "true" ]]; then
-    # If push is set to true, perform the scan with a specific configuration
-    trivy image "$image" \
-      --db-repository choreoanonymouspullable.azurecr.io/aquasecurity/trivy-db:2 \
-      --java-db-repository choreoanonymouspullable.azurecr.io/aquasecurity/trivy-java-db:1 \
-      --exit-code 1 --severity CRITICAL --no-progress --scanners vuln
+  # Run Trivy scan and output as a table with only critical vulnerabilities
+  trivy image "$image" \
+    --db-repository choreoanonymouspullable.azurecr.io/aquasecurity/trivy-db:2 \
+    --java-db-repository choreoanonymouspullable.azurecr.io/aquasecurity/trivy-java-db:1 \
+    --exit-code 1 --severity CRITICAL --no-progress --quiet --format table --scanners vuln
+  
+  scan_exit_code=$?
+
+  # If Trivy finds critical vulnerabilities, display a more user-friendly error
+  if [ $scan_exit_code -ne 0 ]; then
+    echo ""
+    echo "***********************************************************************************"
+    echo "[ERROR] Critical vulnerabilities found in image: $image. Please address these issues."
+    echo "***********************************************************************************"
+    exit $scan_exit_code
   else
-    # Echo the Trivy scan results to the console
-    trivy image "$image" \
-      --db-repository choreoanonymouspullable.azurecr.io/aquasecurity/trivy-db:2 \
-      --java-db-repository choreoanonymouspullable.azurecr.io/aquasecurity/trivy-java-db:1 \
-      --exit-code 1 --severity CRITICAL --no-progress --scanners vuln
+    echo "[SUCCESS] No critical vulnerabilities found in image: $image."
   fi
 }
 
 # Reading command line arguments
 image=$1
-push=${2:-false}
 
 # Running the Trivy scan function
 trivy_scan "$image"
